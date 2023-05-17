65db7c88e2a7df1b83d3c04a28daf40cf3827dd7
Miroslav Tisma <tisma.etf@gmail.com>
2023-05-17 23:12:18 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ wat2wasm wasm-modules/hello.wat -o target/hello.wasm
 wat2wasm wasm-modules/goenv.wat -o target/goenv.wasm
 tinygo build -o target/hello.wasm -target=wasi wasm-modules/hellogo/hellogo.go
 tinygo build -o target/goenv.wasm -target=wasi wasm-modules/goenv/goenv.go
+
+(cd wasm-modules/rustevn; echo $PWD; cargo build --target wasm32-wasi --release)
+cp wasm-modules/rustevn/target/wasm32-wasi/release/rustevn.wasm target/
