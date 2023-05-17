33e1b0d7ace4001d46c8e2b3bb45d5742bec11e1
Miroslav Tisma <tisma.etf@gmail.com>
2023-05-17 11:45:28 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ set -o pipefail
 
 mkdir -p target
 wat2wasm wasm-modules/hello.wat -o target/hello.wasm
+wat2wasm wasm-modules/goenv.wat -o target/goenv.wasm
 tinygo build -o target/hello.wasm -target=wasi wasm-modules/hellogo/hellogo.go
+tinygo build -o target/goenv.wasm -target=wasi wasm-modules/goenv/goenv.go
